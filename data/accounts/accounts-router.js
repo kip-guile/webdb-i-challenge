c7db06f3375524f4d4314983f0857870e0089b6a
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2019-11-04 14:40:40 +0100

    get single account and post endpoints

--- data/accounts/accounts-router.js
+++ data/accounts/accounts-router.js
@@ -12,6 +12,29 @@ router.get('/', (req, res) => {
       .catch(error => {
         res.status(500).json({ message: 'this went wrong: ' + error.message });
       });
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+      const result = await db('accounts').where({ id: req.params.id });
+      res.json(result[0]);
+    } catch (error) {
+      res.status(500).json({ message: 'this went wrong: ' + error.message });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+      const result = await db('accounts')
+        .insert({
+          name: req.body.name,
+          budget: req.body.budget,
+        })
+      res.json('New account got created with an id of ' + result[0]);
+    } catch (error) {
+      res.status(500).json({ message: 'this went wrong: ' + error.message });
+    }
   });
 
-  module.exports = router;
+
+module.exports = router;
